d547d56272866a4f82e263dceda1b07b94c5bc04
tlindsay <tlindsay@una.edu>
2014-02-24 21:46:54 -0600

    Initial Commit

---
+++ ColorPicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorPicker
+//
+//  Created by Patrick Lindsay on 2/24/14.
+//  Copyright (c) 2014 Patrick Lindsay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ColorPicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ColorPicker
+//
+//  Created by Patrick Lindsay on 2/24/14.
+//  Copyright (c) 2014 Patrick Lindsay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
